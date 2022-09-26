51db7d6e446ee9e2f07aff7d21538d371ed248d3
MaximeCoignus <maxime.coignus@gmail.com>
2022-09-25 21:25:29 -0400

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus,
+        voluptas minima deleniti tempora obcaecati unde dolores. A, accusantium.
+        Aliquid eveniet, harum totam ad, numquam, eius et cum optio inventore
+        qui laboriosam libero necessitatibus perspiciatis quae alias quos.
+        Dolores, quidem tenetur!
+      </p>
+    </section>
+  );
+};
+
+export default About;
